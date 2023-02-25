27911e199ae0eabfc2faf37da0c912afd8467ae1
Appi Reddy <appireddy.777@outlook.com>
2023-02-25 12:19:08 +0530

    print todays date

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,3 +6,10 @@ echo "Hello. Good Morning, Todays date is ${DATE}"
 DATE=$(date)
 
 echo "Hello. Good Morning, Todays date is ${DATE}"
+
+## Arthematic Substitution ###
+
+
+ADD=$((2+3))
+
+echo "Addition of 2+3 = ${ADD}"
