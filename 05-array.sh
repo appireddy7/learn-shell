4f4338c37249ab3c5b63816f29344d1c0aba352e
Appi Reddy <appireddy.777@outlook.com>
2023-02-25 12:46:55 +0530

    arrays printing

---
+++ 05-array.sh
@@ -0,0 +1,12 @@
+b=(10 20 39 56 897 7568 98567)
+
+echo "To print the first number ${b[0]}"
+
+echo "To print the Second number ${b[1]}"
+
+echo "To print the fourth number ${b[3]}"
+
+echo "To print the sixth number ${b[5]}"
+
+echo "To print the seventh number ${b[6]}"
+
