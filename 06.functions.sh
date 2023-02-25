99a12e07c74192453413363b38c27e81178af22f
Appi Reddy <appireddy.777@outlook.com>
2023-02-25 17:17:01 +0530

    functions with Arguments

--- 06.functions.sh
+++ 06.functions.sh
@@ -2,8 +2,13 @@
 
 xyz() {
   echo "Hello from function"
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
 }
 
+# Main Program ##
 ##### Call function ####
 
-xyz
+xyz 123 456
