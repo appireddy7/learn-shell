554bfe35f6ac267b0098435e24350ed919948cf5
Appi Reddy <appireddy.777@outlook.com>
2023-02-25 17:36:22 +0530

    functions with Arguments

--- 06.functions.sh
+++ 06.functions.sh
@@ -6,9 +6,13 @@ xyz() {
   echo Second Argument - $2
   echo All Arguments - $*
   echo No of Arguments - $#
+  echo value of a=$a
+  b=200
 }
 
 # Main Program ##
 ##### Call function ####
 
 xyz 123 456
+a=120
+echo value of b=$b
