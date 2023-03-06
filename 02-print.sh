ede439f4ec400688d2cf28319c2e53e364bcf954
Appi Reddy <appireddy.777@outlook.com>
2023-03-06 19:04:35 +0530

    print command practice

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,8 @@
 echo -e "\e[31mHello in Red Color \e[0m"
 echo Hello World
+
+### Some Times we need to print multiple lines ####
+
+echo -e "line1\nline2\nline3"
+
+#### \e for color, \n for new line ####
